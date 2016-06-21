9ccc37908c9e81f0c87603f45ea4ca242378a9f4
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-06-21 12:30:55 -0300

    First version without warnings and errors.

--- index.js
+++ index.js
@@ -1,38 +1,80 @@
 var main = function () {
+    /**
+     * Constructor that initialize all required properties for OverlayMobile class.
+     */
     function OverlayMobile() {
         this.currentUserAgent = window.navigator.userAgent;
         this.mobileDetect = new MobileDetect(this.currentUserAgent);
         this.timeout = null;
+        this.minimumNumberOfTouches = Math.round(document.body.clientHeight / screen.height);
+        this.amountOfTouches = 0;
+        this.touchesTrack = [];
     }
 
-    var showOverlay = function () {
-        $('html, body').animate({ scrollTop: 0 }, "slow");
+    /**
+     * The unique instance for OverlayMobile class.
+     */
+    var instance;
+
+    /**
+     * The method to get/create an unique instance of OverlayMobile class.
+     * @returns The unique instance for OverlayMobile class.
+     */
+    var getInstance = function () {
+        if (!instance) {
+            instance = new OverlayMobile();
+        }
+
+        return instance;
+    };
+
+    OverlayMobile.prototype.showOverlay = function () {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
         $('#overlay').show();
     };
 
     OverlayMobile.prototype.updateStatus = function (e, touch) {
-        $('#cursor-status').text(e.type);
+        $('#cursor-status').text(e.type, touch.currentTouch);
     };
 
-    OverlayMobile.prototype.registerTouch = function (e, touch) {
-        var self = this;
-
+    OverlayMobile.prototype.logTouch = function (e, touch) {
         console.log(e.type, touch);
+    };
 
-        clearTimeout(self.timeout);
+    OverlayMobile.prototype.registerTouch = function () {
+        instance.touchesTrack.push(new Date());
+        instance.amountOfTouches++;
 
-        self.timeout = setTimeout(showOverlay, 5000);
+        console.log(instance.minimumNumberOfTouches);
+
+        if (instance.touchesTrack.length == instance.minimumNumberOfTouches) {
+            clearTimeout(instance.timeout);
+
+            var j = 0;
+            var totalAmount = 0;
+
+            for (var i = 0, k = 1; k < instance.amountOfTouches; i++, k++) {
+                totalAmount += instance.touchesTrack[i].getTime() - instance.touchesTrack[k].getTime();
+
+                j++;
+            }
+
+            var timeToShow = totalAmount / j;
+
+            console.log(timeToShow);
+
+            instance.timeout = setTimeout(instance.showOverlay, timeToShow);
+        }
     };
 
     OverlayMobile.prototype.init = function () {
-        var self = this;
-
-        if (self.mobileDetect.mobile()) {
+        if (instance.mobileDetect.mobile()) {
             var bindable = $(document).Touchable();
 
             bindable
-                .bind('touchablemove touchableend tap longTap doubleTap', self.updateStatus)
-                .bind('touchableend', self.registerTouch);
+                .bind('touchableend', instance.registerTouch)
+                .bind('touchableend', instance.logTouch)
+                .bind('touchablemove touchableend tap longTap doubleTap', instance.updateStatus);
 
             document.addEventListener('focus', function () {
                 $('#tab-status').text = 'focus';
@@ -45,12 +87,12 @@ var main = function () {
     };
 
     return {
-        OverlayMobile: OverlayMobile
+        getInstance: getInstance
     }
 }();
 
 document.addEventListener('DOMContentLoaded', function () {
-    var overlayMobile = new main.OverlayMobile();
+    var overlayMobile = new main.getInstance();
 
     overlayMobile.init();
 });
